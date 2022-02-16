8ed33b99376632aed15f439619732478f8f0ca55
Natalia Pechatnova <pechatnova.n@yandex.ru>
2022-02-17 01:11:03 +0300

    create square and loaded

--- script.js
+++ script.js
@@ -7,30 +7,35 @@ function DomElement (selector, height, width, bg, fontSize) {
     this.bg = bg;
     this.fontSize = fontSize;
     this.createElement = function () {
-        if (this.selector.trim()[0] === ".") {
-            let div = document.createElement('div');
-            div.classList.add(`${selector.slice(1)}`);
-            document.body.append(div);
-            div.innerText = "Здесь создан элемент div. С другой стороны, глубокий уровень погружения не оставляет шанса для  "
-            div.style.cssText=`height: ${height  + 'px'};
-                width: ${width  + 'px'};
-                background-color: ${bg};
-                font-size : ${fontSize  + 'px'}; `;
-            console.log(div)
-        } else if (this.selector.trim()[0] === "#") {
-            let paragraph = document.createElement('p');
-            paragraph.setAttribute("id", `${selector.slice(1)}`);
-            document.body.append(paragraph);
-            paragraph.innerText = "Здесь создан элемент p. Учитывая ключевые сценарии поведения, современная методология разработки говорит о возможностях поставленных обществом задач."
-            paragraph.style.cssText=`height: ${height + 'px'};
-                width: ${width  + 'px'};
-                background-color: ${bg};
-                font-size : ${fontSize  + 'px'}; `;
-            console.log(paragraph);
-        }
 
+        document.addEventListener('DOMContentLoaded', () => {
+            if (this.selector.trim()[0] === ".") {
+                let div = document.createElement('div');
+                div.classList.add(`${selector.slice(1)}`);
+                document.body.append(div);
+                div.innerText = "Здесь создан элемент div";
+                div.style.cssText=`height: ${height  + 'px'};
+                width: ${width  + 'px'};
+                background-color: ${bg};
+                position: absolute;
+                font-size : ${fontSize  + 'px'}; `;
+                console.log(div)
+            } else if (this.selector.trim()[0] === "#") {
+                let paragraph = document.createElement('p');
+                paragraph.setAttribute("id", `${selector.slice(1)}`);
+                document.body.append(paragraph);
+                paragraph.innerText = "Здесь создан элемент p";
+                paragraph.style.cssText=`height: ${height + 'px'};
+                width: ${width  + 'px'};
+                background-color: ${bg};
+                position: absolute;
+                font-size : ${fontSize  + 'px'}; `;
+                console.log(paragraph);
+            }
+        });
     }
+
 }
 
-let elem = new DomElement('.block', '100', '200', 'pink', '18');
+let elem = new DomElement('.block', '100', '100', 'pink', '18');
 elem.createElement();
