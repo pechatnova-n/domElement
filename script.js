1efe24347fb3c74deec39d4312b60faee06b0dc5
Natalia Pechatnova <pechatnova.n@yandex.ru>
2022-02-17 00:52:27 +0300

    lesson15

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function DomElement (selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.createElement = function () {
+        if (this.selector.trim()[0] === ".") {
+            let div = document.createElement('div');
+            div.classList.add(`${selector.slice(1)}`);
+            document.body.append(div);
+            div.innerText = "Здесь создан элемент div. С другой стороны, глубокий уровень погружения не оставляет шанса для  "
+            div.style.cssText=`height: ${height  + 'px'};
+                width: ${width  + 'px'};
+                background-color: ${bg};
+                font-size : ${fontSize  + 'px'}; `;
+            console.log(div)
+        } else if (this.selector.trim()[0] === "#") {
+            let paragraph = document.createElement('p');
+            paragraph.setAttribute("id", `${selector.slice(1)}`);
+            document.body.append(paragraph);
+            paragraph.innerText = "Здесь создан элемент p. Учитывая ключевые сценарии поведения, современная методология разработки говорит о возможностях поставленных обществом задач."
+            paragraph.style.cssText=`height: ${height + 'px'};
+                width: ${width  + 'px'};
+                background-color: ${bg};
+                font-size : ${fontSize  + 'px'}; `;
+            console.log(paragraph);
+        }
+
+    }
+}
+
+let elem = new DomElement('.block', '100', '200', 'pink', '18');
+elem.createElement();
